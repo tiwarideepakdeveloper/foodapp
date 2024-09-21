67e202ef6795d6e13d6ec3771abb067c4ad523f4
navjotsingh06 <navjot.s0608@gmail.com>
2024-09-21 12:05:29 +0530

    dashboard link in homepage navbar

--- src/middleware/appMiddleware.js
+++ src/middleware/appMiddleware.js
@@ -8,6 +8,7 @@ export const appData = (req, res, next) => {
     req.langCode = req.headers['language-code'] || 'en';
     req.currencyCode = req.headers['currency-code'] || 'INR';
     req.timeZone = req.headers['time-zone'] || 'UTC';
+    res.locals.loggedUser = req.session.user;
     next();
 }
 
